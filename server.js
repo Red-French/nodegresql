2cebcb2274ea80d4d94ccc11e4810f84ff175ea3
Red-French <red@redfrench.net>
2020-11-15 10:29:15 -0600

    2nd commit

---
+++ server.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const express = require('express');
+
+const models = require('./models/');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({
+    status: 'Success'
+  });
+});
+
+app.get('/genres', (req, res) => {
+  models.Genre.findAll()
+    .then(genres => res.send(genres));
+});
+
+app.get('/mediatypes', (req, res) => {
+  models.MediaType.findAll()
+    .then(types => res.send(types));
+});
+
+app.get('/artists', (req, res) => {
+  models.Artist.findAll()
+    .then(artists => res.send(artists));
+});
+
+app.get('/playlists', (req, res) => {
+  models.Playlist.findAll()
+    .then(playlists => res.send(playlists));
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
